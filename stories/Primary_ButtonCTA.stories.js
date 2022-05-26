1811e3005c183cb37f8ae6f229990c694ca45f28
Jordan Linton <jordaneddielinton93@hotmail.co.uk>
2022-05-27 00:07:47 +0100

    inistial commit packages

---
+++ stories/Primary_ButtonCTA.stories.js
@@ -0,0 +1,10 @@
+import Primary_ButtonCTA from "../Components/Buttons/Primary_ButtonCTA";
+
+export default {
+  title: "Button",
+  component: Primary_ButtonCTA,
+};
+
+export const PrimarButtonCTA = () => (
+  <Primary_ButtonCTA label="press me" backgroundColor="red" />
+);
