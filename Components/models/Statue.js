4c61af2d3a7d5a1a676df6190df9ba8df6bebf67
Jordan Linton <jordaneddielinton93@hotmail.co.uk>
2022-06-02 23:54:23 +0100

    replaced model remved faces from 3d model

--- Components/models/Statue.js
+++ Components/models/Statue.js
@@ -12,17 +12,18 @@ const Statue = ({}) => {
   useFrame((state, delta) => {
     // console.log(ref.current.rotation.x)
 
-    ref.current.rotation.x = 3.1;
-    ref.current.rotation.z = 3.1;
+    ref.current.rotation.z = 0;
     // ref.current.rotation.y = 300;
     if (rotate) {
-      // ref.current.rotation.x += 0.005;
-      ref.current.rotation.y += 0.004;
-      // ref.current.rotation.x > 4 && setRotate(false);
+      ref.current.rotation.y += 0.005;
+      ref.current.rotation.y > 1.5 && setRotate(false);
+    } else {
+      ref.current.rotation.y -= 0.005;
+      ref.current.rotation.y < -1 && setRotate(true);
     }
   });
   return (
-    <mesh ref={ref} position={[0, -1, 2.2]} scale={0.002}>
+    <mesh ref={ref} position={[0, -1, 2.2]} scale={11}>
       <primitive object={gltf.scene} />
     </mesh>
   );
