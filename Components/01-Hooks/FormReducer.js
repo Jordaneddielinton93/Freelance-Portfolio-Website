b7fa77b0e003ee719f244a9113403a4b9eb28115
Jordan Linton <jordaneddielinton93@hotmail.co.uk>
2022-06-14 05:06:17 +0100

    Added responsivness to the projects page

--- Components/01-Hooks/FormReducer.js
+++ Components/01-Hooks/FormReducer.js
@@ -10,30 +10,7 @@ export const inistialState = {
 
 export const FormReducer = (state, action) => {
   switch (action.type) {
-    // case "FormValidation":
-    //   if ((state.ProjectType == "Website") && (state.DevOrDeisgnAndDev.length != 0) && (state.Budget.length != 0) && (state.Timeline.length != 0) && (state.Fullname.length != 0) && (state.Email.length != 0)) {
-    //     alert("Website filled in")
 
-    //   }
-
-    //   if ((state.ProjectType == "Tutoring") && (state.Budget.length != 0) && (state.Timeline.length != 0) && (state.Fullname.length != 0) && (state.Email.length != 0)) {
-
-    //     alert("Tutoring filled in")
-    //   }
-    //   if ((state.ProjectType == "Sponsor") && (state.Budget.length != 0) && (state.Fullname.length != 0) && (state.Email.length != 0)) {
-    //     alert("sponsor filled in")
-
-    //   }
-    //   if ((state.ProjectType == "Article") && (state.Fullname.length != 0) && (state.Email.length != 0)) {
-    //     return alert("Article filled in")
-    //   }
-    //   // 
-
-
-
-    //   return {
-    //     ...state
-    //   };
     case "Reset":
       return {
         ProjectType: "",
---
+++ Components/SkillsAndProject/SkillsAndProject.js
@@ -0,0 +1,44 @@
+import Phone1 from "../PhoneShell/Phone1";
+import Skills from "../Skills/Skills";
+import style from "./SkillsAndProject.module.css"
+const SkillsAndProject = () => {
+  return (
+    <main className={style.Container}>
+      <div className={style.Container_Main}>
+        <Skills ListHeading={"Hard Skills"} listItems={[
+          "UX Discovery",
+          "HTML",
+          "CSS/SASS/Modules",
+          " React & Next.js",
+          "Styled Components",
+          "Node.js",
+          "PostgreSQL",
+          "Jest/Testing",
+          "Strapi CMS",
+          "Figma"
+        ]} />
+
+        <Phone1
+          img1="/images/phone3/img1.png"
+          img2="/images/phone3/img2.png"
+          img3="/images/phone3/img3.png"
+          img4="/images/phone3/img4.png"
+        />
+
+        <Skills ListHeading={"Soft Skills"} listItems={[
+          "Agile Methodologies",
+          " Disney Ideation",
+          "Jira Workflow",
+          " User Personas",
+          "User Flows",
+          "Site Maps",
+          "Wireframes",
+
+        ]} />
+
+      </div>
+    </main>
+  );
+}
+
+export default SkillsAndProject;
