f59df3ab4db57a467181c44252c54d93251c20a4
Jordan Linton <jordaneddielinton93@hotmail.co.uk>
2022-06-17 08:22:44 +0100

    added images to the cards on the article page

--- Components/Card/Card.js
+++ Components/Card/Card.js
@@ -10,7 +10,7 @@ const Card = ({ attributes }) => {
     >
       <section className={style.Card_infoBox}>
         <div className={style.Card_infoBox_img}>
-          <Image src={"/navLogo.png"} width="70px" height="70px" alt="Card img" />
+          <Image src={"/images/greyback.png"} width="70px" height="70px" alt="Card img" />
         </div>
         <h4>Title: {"Card_Title"}</h4>
         <h4>Author: {"Card_Painter"}</h4>
