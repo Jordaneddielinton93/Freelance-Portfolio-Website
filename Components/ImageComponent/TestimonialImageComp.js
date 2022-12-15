6f9e3ec9925c5a01b70c8a1b8cbdeaf527a38f12
School Of Code Staff <staff@KDP4PCFLWR.local>
2022-12-15 06:08:09 +0000

    updated project page, also trimmed my picture to fit better

--- Components/ImageComponent/TestimonialImageComp.js
+++ Components/ImageComponent/TestimonialImageComp.js
@@ -1,13 +1,16 @@
 import Image from "next/image";
+import Link from "next/link";
 import styled from "./Testimonial.module.css";
 const TestimonialImageComp = ({ src, top, right, delay }) => {
   return (
-    <span
-      className={styled.Container}
-      style={{ right: right, top: top, zIndex: 10, animationDelay: delay }}
-    >
-      <Image height="94px" width="223px" alt="Initials" src={src} />
-    </span>
+    <Link href={"/Tutor"}>
+      <span
+        className={styled.Container}
+        style={{ right: right, top: top, zIndex: 10, animationDelay: delay }}
+      >
+        <Image height="94px" width="223px" alt="Initials" src={src} />
+      </span>
+    </Link>
   );
 };
 
